1c5618e8c9933db9b78a8e069976d99d0fe124f9
Tatisen <joel.rossander@gmail.com>
2025-01-28 17:32:45 +0100

    initial commit

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,5 @@
+require("joel")
+require("config.lazy")
+
+
+print ("Hello")
---
+++ nvim/.config/nvim/lua/joel/init.lua
@@ -0,0 +1,6 @@
+require("joel.remap")
+print("Hello from joel")
+
+vim.opt.number = true          -- Enables absolute line numbers
+vim.opt.relativenumber = true  -- Enables relative line numbers
+   
